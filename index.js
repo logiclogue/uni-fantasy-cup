b8d17a0d81f9357cf367cd0f6f76b6c86789471d
logiclogue <jordanpaullord@hotmail.co.uk>
2018-01-29 12:27:43 +0000

    Maps all matches

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const config = require("./config.json");
 const robin = require("roundrobin");
 const teams = require("./src/teams");
 
+// Match -> String
+function matchToString(match) {
+    const home = match.home.name;
+    const away = match.away.name;
+    const score = match.period.goals.value;
+
+    return home + ' ' + score[0] + '-' + score[1] + ' ' + away;
+}
+
+// Seed -> (Team -> Team -> Match)
+function toMatch(seed) {
+    return (home, away) => new Match(home, away, seed);
+}
+
 teams.then(teams => {
     const seed = "testing".toSeed();
     const match = new Match(teams[0], teams[1], seed);
+    const matches = robin(teams.length, teams);
 
-    console.log(robin(teams.length, teams));
+    const results = _(matches)
+        .flatMap(toMatch(seed))
+        .map(matchToString)
+        .value();
 
-    console.log(match.period.goals);
+    console.log(results);
+    console.log(matchToString(match));
 });
