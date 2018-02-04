fcf51a6c37be19f66d5b2e6f5f8dc09fd97c96fb
logiclogue <jordanpaullord@hotmail.co.uk>
2018-02-04 17:39:15 +0000

    Fixes last

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const _ = require("lodash");
 const Match = require("./src/Match");
 const Seed = require("football-score-sim/src/Seed");
+const times = require("football-score-sim/src/times");
+const Time = require("football-score-sim/src/Time");
 const config = require("./config.json");
 const robin = require("roundrobin");
 const teams = require("./src/teams");
@@ -14,6 +16,13 @@ function matchToString(match) {
     return home + ' ' + score[0] + '-' + score[1] + ' ' + away;
 }
 
+// [Match] -> Number -> String
+function roundToString(round, i) {
+    const number = i + 1;
+
+    return "Round " + number + "\n" + round.join("\n");
+}
+
 // Seed -> ([Team] -> Match)
 function toMatch(seed) {
     return teams => new Match(teams, seed);
@@ -21,7 +30,8 @@ function toMatch(seed) {
 
 teams.then(teams => {
     const seed = "testing".toSeed();
-    const match = new Match(teams[0], teams[1], seed);
+    const matchLength = new Time().setMinutes(90);
+    const match = new Match([teams[0], teams[1]], seed);
     const matches = robin(teams.length, teams);
 
     const results = _(matches)
